b20081f1435847d1b48be8822e8888e40fbb0f8f
yan-batista <ybatistasouza@hotmail.com>
2024-05-17 20:00:13 -0300

    added colors on initial input and commands; handling user keyboard interrupt

--- blacksmith.py
+++ blacksmith.py
@@ -9,40 +9,50 @@ parser.add_argument("-n", "--name", help="Select the name of the project", metav
 args = parser.parse_args()
 project_name = args.name
 
+COLOR_BLUE = '\033[34m'
+COLOR_MAGENTA = '\033[35m'
+COLOR_RESET = "\033[0m"
+
 def main():
-  current_directory = os.getcwd()
-  answer = input(f"""This will create a React project named {project_name} on {current_directory}. 
-It will also configure TailwindCSS, ESLint and Prettier. Continue (y/n) ? """) or 'y'
-  if answer.lower() in ['y', "yes"]:
-    print("running")
-    # Commands List
-    commands = [
-      # create react with vite and install dependencies
-      f"npm create vite@latest {project_name} -- --template react-ts",
-      f"cd {project_name} && npm install",
-      f"cd {project_name} && npm install -D tailwindcss postcss autoprefixer prettier eslint @rocketseat/eslint-config",
+  try:
+    current_directory = os.getcwd()
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    config_files_dir = "config_files"
 
-      # configure tailwindcss
-      f"cd {project_name} && npx tailwindcss init",
-      f"copy /Y tailwind.config.js .\\{project_name}",
-      f"copy /Y postcss.config.js .\\{project_name}",
+    answer = input(f"""{COLOR_BLUE}This will create a React project named {project_name} on {current_directory}. 
+It will also configure TailwindCSS, ESLint and Prettier. Continue (y/n) ? {COLOR_RESET}""") or 'y'
+    if answer.lower() in ['y', "yes"]:
+      # Commands List
+      commands = [
+        # create react with vite and install dependencies
+        f"npm create vite@latest {project_name} -- --template react-ts",
+        f"cd {project_name} && npm install",
+        f"cd {project_name} && npm install -D tailwindcss postcss autoprefixer prettier eslint @rocketseat/eslint-config",
 
-      # remove index.css and App.css and updates src folder with base files
-      f"cd {project_name}/src && del index.css && del App.css",
-      f"xcopy src .\\{project_name}\\src /e /i /y",
+        # configure tailwindcss
+        f"cd {project_name} && npx tailwindcss init",
+        f"copy /Y {script_dir}\\{config_files_dir}\\tailwind.config.js .\\{project_name}",
+        f"copy /Y {script_dir}\\{config_files_dir}\\postcss.config.js .\\{project_name}",
 
-      # configure prettier and eslint
-      f"copy /Y .eslintrc.cjs .\\{project_name}",
-      f"copy /Y .prettierrc .\\{project_name}",
-      f"copy /Y .prettierignore .\\{project_name}",
-    ]
+        # remove index.css and App.css and updates src folder with base files
+        f"cd {project_name}\\src && del index.css && del App.css",
+        f"xcopy {script_dir}\\{config_files_dir}\\src .\\{project_name}\\src /e /i /y",
 
-    # Run Commands
-    for command in commands:
-      print(command)
-      subprocess.run(command, shell=True, check=True, cwd=current_directory)
-  else:
-    sys.exit()
+        # configure prettier and eslint
+        f"copy /Y {script_dir}\\{config_files_dir}\\.eslintrc.cjs .\\{project_name}",
+        f"copy /Y {script_dir}\\{config_files_dir}\\.prettierrc .\\{project_name}",
+        f"copy /Y {script_dir}\\{config_files_dir}\\.prettierignore .\\{project_name}",
+      ]
+
+      # Run Commands
+      for command in commands:
+        print(f"{COLOR_MAGENTA}now running: {command}{COLOR_RESET}")
+        subprocess.run(command, shell=True, check=True, cwd=current_directory)
+    else:
+      sys.exit()
+  except KeyboardInterrupt:
+    exit(0)
+
 
 if __name__ == "__main__":
   main()
