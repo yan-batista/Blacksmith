fdccddf7aac9465d8d103ee58f8e6f796e5766bc
yan-batista <ybatistasouza@hotmail.com>
2024-05-18 00:06:59 -0300

    gitignore; fixed pathing

--- blacksmith.py
+++ blacksmith.py
@@ -16,10 +16,14 @@ COLOR_RESET = "\033[0m"
 def main():
   try:
     current_directory = os.getcwd()
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    config_files_dir = "config_files"
+    if getattr(sys, 'frozen', False):
+      script_dir = os.path.dirname(sys.executable)
+      config_files_dir = os.path.join(script_dir, "_internal", "config_files")
+    else:
+      script_dir = os.path.dirname(os.path.realpath(__file__))
+      config_files_dir = os.path.join(script_dir, "config_files")
 
-    answer = input(f"""{COLOR_BLUE}This will create a React project named {project_name} on {current_directory}. 
+    answer = input(f"""{COLOR_BLUE}This will create a React project named {project_name} in {current_directory}. 
 It will also configure TailwindCSS, ESLint and Prettier. Continue (y/n) ? {COLOR_RESET}""") or 'y'
     if answer.lower() in ['y', "yes"]:
       # Commands List
@@ -31,17 +35,17 @@ It will also configure TailwindCSS, ESLint and Prettier. Continue (y/n) ? {COLOR
 
         # configure tailwindcss
         f"cd {project_name} && npx tailwindcss init",
-        f"copy /Y {script_dir}\\{config_files_dir}\\tailwind.config.js .\\{project_name}",
-        f"copy /Y {script_dir}\\{config_files_dir}\\postcss.config.js .\\{project_name}",
+        f"copy {os.path.join(config_files_dir, 'tailwind.config.js')} {os.path.join(current_directory, project_name)}",
+        f"copy {os.path.join(config_files_dir, 'postcss.config.js')} {os.path.join(current_directory, project_name)}",
 
         # remove index.css and App.css and updates src folder with base files
-        f"cd {project_name}\\src && del index.css && del App.css",
-        f"xcopy {script_dir}\\{config_files_dir}\\src .\\{project_name}\\src /e /i /y",
+        f"cd {os.path.join(project_name, 'src')} && del index.css && del App.css",
+        f"xcopy {os.path.join(config_files_dir, 'src')} {os.path.join(current_directory, project_name, 'src')} /e /i /y",
 
         # configure prettier and eslint
-        f"copy /Y {script_dir}\\{config_files_dir}\\.eslintrc.cjs .\\{project_name}",
-        f"copy /Y {script_dir}\\{config_files_dir}\\.prettierrc .\\{project_name}",
-        f"copy /Y {script_dir}\\{config_files_dir}\\.prettierignore .\\{project_name}",
+        f"copy {os.path.join(config_files_dir, '.eslintrc.cjs')} {os.path.join(current_directory, project_name)}",
+        f"copy {os.path.join(config_files_dir, '.prettierrc')} {os.path.join(current_directory, project_name)}",
+        f"copy {os.path.join(config_files_dir, '.prettierignore')} {os.path.join(current_directory, project_name)}",
       ]
 
       # Run Commands
@@ -52,6 +56,9 @@ It will also configure TailwindCSS, ESLint and Prettier. Continue (y/n) ? {COLOR
       sys.exit()
   except KeyboardInterrupt:
     exit(0)
+  except Exception as e:
+    print(f"{COLOR_MAGENTA}An error occurred: {e}{COLOR_RESET}")
+    sys.exit(1)
 
 
 if __name__ == "__main__":
